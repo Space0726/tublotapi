8f720c33a686a29bedd87a70d475bcf00d643f31
Space0726 <space.sjwoo@gmail.com>
2020-03-30 21:23:14 +0900

    Change DBMS SQLite to MySQL

--- tublotapi/settings.py
+++ tublotapi/settings.py
@@ -87,12 +87,16 @@ CORS_ORIGIN_WHITELIST = [
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+from . import mysql_settings
+
+DATABASES = mysql_settings.DATABASES
+
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
 
 # Password validation
